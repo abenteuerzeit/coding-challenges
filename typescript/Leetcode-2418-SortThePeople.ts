bfa98ab3ab23d106eee86b7be35dfa57f938be05
abenteuerzeit <dr.adrian.mroz@outlook.com>
2024-07-23 00:03:00 +0200

    ♻️ Refactor

--- typescript/Leetcode-2418-SortThePeople.ts
+++ typescript/Leetcode-2418-SortThePeople.ts
@@ -1,33 +1,11 @@
-class Person {
-    #name: string;
-    #height: number;
-
-    constructor(name: string, height: number) {
-        this.#name = name;
-        this.#height = height;
-    }
-
-    getHeight(): number {
-        return this.#height;
-    }
-
-    getName(): string {
-        return this.#name;
-    }
-
-    static byHeightDesc(a: Person, b: Person): number {
-        return a.getHeight() < b.getHeight() ? 1 : -1
-    }
-}
-
+interface Person {
+    name: string;
+    height: number;
+};
 
 function sortPeople(names: string[], heights: number[]): string[] {
-    const people: Person[] = [];
-    for (let i = 0; i < names.length; ++i) {
-        const person: Person = new Person(names[i], heights[i]);
-        people.push(person);
-    }
-
-    people.sort(Person.byHeightDesc);
-    return people.map(p => p.getName());
-};
+    return names
+        .map((name, i) => ({ name, height: heights[i] } as Person))
+        .sort((a, b) => b.height - a.height)
+        .map(person => person.name);
+}
